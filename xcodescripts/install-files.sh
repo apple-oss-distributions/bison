ce85eb91324279bbbcbb438cad0e33db9faae50b
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2023-10-03 18:15:37 +0000

    bison-15
    Imported from bison-15.tar.gz

--- xcodescripts/install-files.sh
+++ xcodescripts/install-files.sh
@@ -1,13 +1,8 @@
 set -e -x
 
-BINDIR="$DSTROOT/$DT_TOOLCHAIN_DIR"/usr/bin
-install -c -o root -g wheel -m 0755 \
-	"$PROJECT_DIR"/yacc.sh \
-	"$BINDIR"/yacc
-
 SHAREDIR="$DSTROOT/$DT_TOOLCHAIN_DIR"/usr/share/bison
-install -d -o root -g wheel -m 0755 "$SHAREDIR"
-install -c -o root -g wheel -m 0644 \
+install -d -m 0755 "$SHAREDIR"
+install -c -m 0644 \
 	"$PROJECT_DIR"/data/c++.m4 \
 	"$PROJECT_DIR"/data/c.m4 \
 	"$PROJECT_DIR"/data/glr.c \
@@ -17,27 +12,24 @@ install -c -o root -g wheel -m 0644 \
 	"$PROJECT_DIR"/data/README \
 	"$PROJECT_DIR"/data/yacc.c \
 	"$SHAREDIR"
-install -d -o root -g wheel -m 0755 "$SHAREDIR"/m4sugar
-install -c -o root -g wheel -m 0644 \
+install -d -m 0755 "$SHAREDIR"/m4sugar
+install -c -m 0644 \
 	"$PROJECT_DIR"/data/m4sugar/m4sugar.m4 \
 	"$SHAREDIR"/m4sugar
 
 MANDIR="$DSTROOT/$DT_TOOLCHAIN_DIR"/usr/share/man/man1
-install -d -o root -g wheel -m 0755 "$MANDIR"
-install -c -o root -g wheel -m 0644 \
+install -d -m 0755 "$MANDIR"
+install -c -m 0644 \
 	"$PROJECT_DIR"/doc/bison.1 \
-	"$MANDIR"
-install -c -o root -g wheel -m 0644 \
-	"$PROJECT_DIR"/yacc.1 \
 	"$MANDIR"
 
 OSV="$DSTROOT"/usr/local/OpenSourceVersions
 OSL="$DSTROOT"/usr/local/OpenSourceLicenses
-install -d -o root -g wheel -m 0755 "$OSV"
-install -c -o root -g wheel -m 0644 \
+install -d -m 0755 "$OSV"
+install -c -m 0644 \
 	"$PROJECT_DIR"/bison.plist \
 	"$OSV"
-install -d -o root -g wheel -m 0755 "$OSL"
-install -c -o root -g wheel -m 0644 \
+install -d -m 0755 "$OSL"
+install -c -m 0644 \
 	"$PROJECT_DIR"/COPYING \
 	"$OSL"/bison.txt
